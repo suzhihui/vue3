30c50c381d9f284ff716e656e02b8b4178c8e8e4
suzhihui <wisdom.su@163.com>
2021-08-12 11:07:00 +0800

    chang reative

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,50 @@
   <h2>年龄：{{age}}</h2>
   <h3>工种：{{job.type}}</h3>
   <h3>薪水: {{job.salary}}</h3>
+  <h3>工号: {{job.number}}</h3>
+  <h3>爱好: {{hobby}}</h3>
+  <h3>a/b/c/d: {{job.a.b.c.d}}</h3>
   <button @click="changeInfo">修改牛人的信息</button>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import { ref, reactive } from '@vue/reactivity';
 export default {
     name: 'App',
     setup() {
         let age = ref(36);
         let name = ref('老苏');
-        let job = ref({
+        let job = reactive({
             type: '前端工程师',
-            salary: '30K'
+            salary: '30K',
+            number: 666,
+            a: {
+                b: {
+                    c: {
+                        d: 100
+                    }
+                }
+            }
         })
+        let hobby = reactive(['抽烟', '喝酒', '烫头'])
+        // let number = reactive(666)
         function changeInfo() {
-            name.value = '小苏'
-            age.value = 18
-            job.value.type = 'UI设计师'
-            job.value.salary = '60K'
-            console.log(name, age)
+            // console.log(number)
+            // name.value = '小苏'
+            // age.value = 18
+            // job.type = 'UI设计师'
+            // job.salary = '60K'
+            // // console.log(name, age)
+            // job.number = 999
+            // job.a.b.c.d = 200
+            hobby[0] = '学习' // vue 不能用索引改
         }
         return {
             age,
             name,
             job,
-            changeInfo
+            hobby,
+            changeInfo,
         }
     }
 }
